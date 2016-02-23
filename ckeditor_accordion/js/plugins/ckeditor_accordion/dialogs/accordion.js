27ea9dbd7e98d8781358eaaac9dda0d700625e6d
Adam Giacomelli <adamg@agialab.com>
2016-02-23 12:05:34 +0100

    Added commented-out foundation implementation

--- ckeditor_accordion/js/plugins/ckeditor_accordion/dialogs/accordion.js
+++ ckeditor_accordion/js/plugins/ckeditor_accordion/dialogs/accordion.js
@@ -9,7 +9,7 @@ CKEDITOR.dialog.add( 'accordionDialog', function ( editor ) {
                 label: 'Basic Settings',
                 elements: [
                     {
-                        type: 'text',
+                        type: 'text'    ,
                         id: 'number',
                         label: 'Number of sections',
                         validate: CKEDITOR.dialog.validate.notEmpty( Drupal.t("The field cannot be empty."))
@@ -28,7 +28,30 @@ CKEDITOR.dialog.add( 'accordionDialog', function ( editor ) {
             }
 
             editor.insertHtml('<div class="accordion">'+ intern +'</div>');
-
+            /*Foundation implementation */
+            /*
+                For foundation equalizer support -
+                $('.accordion-reflow').on('toggled', function (event, accordion) {
+                    $(document).foundation('equalizer', 'reflow');
+                    console.log(accordion);
+                });
+            */
+            /*
+                sectionHeader = "<li class='accordion-navigation'>";
+                sectionFooter = "</li>";
+    
+                intern = "";
+    
+                for (i=0;i<sections;i++){
+                    section =
+                    sectionHeader
+                    + '<a href="#panel'+i+'a">Accordion 1</a><div id="panel'+i+'a" class="content">Text</div>'
+                    + sectionFooter;
+                    intern = intern + section
+                }
+    
+                editor.insertHtml('<ul class="accordion accordion-reflow" data-accordion>'+ intern +'</ul>');
+            */
         }
     };
 });
